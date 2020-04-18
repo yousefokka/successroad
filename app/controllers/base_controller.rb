3bedc01e4d88458651cf24964617fd9f41dd72d2
Maghraby <maghraby.fcih@gmail.com>
2020-04-18 20:27:49 +0000

    Use rails symbols

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,10 +1,10 @@
 class BaseController < ApplicationController
   rescue_from ActionController::ParameterMissing do |exception|
-    render json: {error: {message: exception.message}}, status: 400
+    render json: {error: {message: exception.message}}, status: :bad_request
   end
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: {error: {message: exception.message}}, status: 404
+    render json: {error: {message: exception.message}}, status: :not_found
   end
 end
 
