f56aab59b195baaa40a7418bfa6fe4f691184f43
Maghraby <maghraby.fcih@gmail.com>
2020-04-18 20:24:28 +0000

    Handle 404 record not found

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,5 +2,9 @@ class BaseController < ApplicationController
   rescue_from ActionController::ParameterMissing do |exception|
     render json: {error: {message: exception.message}}, status: 400
   end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: {error: {message: exception.message}}, status: 404
+  end
 end
 
